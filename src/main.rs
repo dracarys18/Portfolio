84b4e45fd5ab38a5f46c4d6d7350d3cb2095c900
dracarys18 <karthihegde010@gmail.com>
2021-08-22 22:03:03 +0530

    Heroku Setup
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 extern crate rocket;
 mod temp;
 
-use std::io::Cursor;
+use std::{ffi::OsStr, io::Cursor, path::PathBuf};
 
 use crate::temp::PostIndex;
 use rocket::{http::ContentType, response};
@@ -35,16 +35,34 @@ fn get_article(file: String) -> Template {
 }
 #[get("/favicon.ico")]
 fn favicon<'f>() -> response::Result<'f> {
-    let fav = std::fs::read("static/favicon.ico").unwrap();
+    let fav = std::fs::read(format!(
+        "{}/static/favicon.ico",
+        std::env::current_dir().unwrap().display()
+    ))
+    .unwrap();
     response::Response::build()
         .header(ContentType::Icon)
         .sized_body(Cursor::new(fav))
         .ok()
 }
+#[get("/static/<file..>")]
+fn get_css<'c>(file: PathBuf) -> response::Result<'c> {
+    let ext = file.as_path().extension().and_then(OsStr::to_str).unwrap();
+    let content = std::fs::read_to_string(format!(
+        "{}/static/{}",
+        std::env::current_dir().unwrap().display(),
+        file.display().to_string()
+    ))
+    .unwrap();
+    response::Response::build()
+        .header(ContentType::from_extension(ext).unwrap())
+        .sized_body(Cursor::new(content))
+        .ok()
+}
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, favicon, blog, get_article])
+        .mount("/", routes![index, favicon, blog, get_article, get_css])
         .attach(Template::fairing())
         .mount(
             "/static",
